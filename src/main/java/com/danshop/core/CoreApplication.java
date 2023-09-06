a3de604d009e05c256a0072612ab90dc19c677c2
Daniele Parisi <neb.inf@gmail.com>
2023-09-06 17:49:34 +0200

    Add Observability: tracing / metrics. Add Prometheus and Zipkin configuration

--- src/main/java/com/danshop/core/CoreApplication.java
+++ src/main/java/com/danshop/core/CoreApplication.java
@@ -1,9 +1,11 @@
 package com.danshop.core;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
@@ -32,6 +34,14 @@ public class CoreApplication {
         return args -> {
             MDC.put(MDC_APPLICATION_NAME, serviceName);
         };
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags(Environment environment) {
+        String serviceName = environment.getProperty("spring.application.name");
+
+        /* Exposing application name to Prometheus, so that it's shown on Grafana */
+        return registry -> registry.config().commonTags("application", serviceName);
     }
 
 }
